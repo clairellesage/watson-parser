b194512ed33000cb8e5ce4b14102295e1dd665c1
Claire Lesage <clairellesage@gmail.com>
2017-03-15 20:05:26 +0000

    can be document text/scores and keywords text/scores for an indivdual question

--- parser.js
+++ parser.js
@@ -1,48 +1,62 @@
  const data = require("./data.json")
 
-
+//input any question, get the correct column data
 function getQuestion(columnNumber, data) {
 	for (i in data) {
 		for (j in data[i].watson) {
-			//columnNumber needs to be subbed in here for column
 			let participant = data[i].watson[j]
-			for (q in participant) {
-				participant['column-idx'] = columnNumber
-				let question = participant[q]
-				return question;
+			if (participant.analysis) {
+				let documentText = participant.text;
+				let documentScore = participant.analysis.sentiment.document.score
+				// console.log(`${documentText}; ${documentScore}`)
 			}
+				participant['column-idx'] = columnNumber
+				// console.log(participant)
+				if (participant.analysis) {
+					let keywords = participant.analysis.keywords
+					// console.log(keywords)
+					for (word in keywords) {
+						let keywordText = keywords[word].text;
+						let keywordScore = keywords[word].sentiment.score
+						console.log(`${keywordText}; ${keywordScore}`)					
+					}
+					// console.log(participant.analysis.keywords)
+					let text = participant.analysis.keywords
+					let keywordsObj = {"word": text, "score": participant.analysis.keywords}
+				}
+
 		}
 	}
 }
 
-// function keyValues(data) { 
-// 	const sentimentArr = []
-// 	let sentimentAnalysis = data[i].watson[j]
-// 	for (k in sentimentAnalysis){
-// 		if (sentimentAnalysis[k]) {
-// 			let comment = sentimentAnalysis[k].keywords
-// 			for (m in comment) {
-// 				let sentimentObj = {text: comment[m].text, score: comment[m].sentiment.score}
-// 				sentimentArr.push(sentimentObj);
-// 			}
-// 			sortValues(sentimentArr);
-// 		}
-// 	}
-// }
+function keyValues(question) { 
+	const sentimentArr = [];
+	console.log(question)
+	// for (k in sentimentAnalysis){
+	// 	if (sentimentAnalysis[k]) {
+	// 		let comment = sentimentAnalysis[k].keywords
+	// 		for (m in comment) {
+	// 			let sentimentObj = {text: comment[m].text, score: comment[m].sentiment.score}
+	// 			sentimentArr.push(sentimentObj);
+	// 		}
+	// 		sortValues(sentimentArr);
+	// 	}
+	// }
+}
 
-// function compare(a,b) {
-//   if (a.score < b.score)
-//     return -1;
-//   if (a.score > b.score)
-//     return 1;
-//   return 0;
-// }
+function compare(a,b) {
+  if (a.score < b.score)
+    return -1;
+  if (a.score > b.score)
+    return 1;
+  return 0;
+}
 
-// function sortValues(arr){
-// 	let concatedArr = [].concat.apply([], arr)
-// 	let sortedValues = arr.sort(compare)
-// 	console.log(sortedValues)
-// 	// console.log(concatedArr)
-// }
+function sortValues(arr){
+	let concatedArr = [].concat.apply([], arr)
+	let sortedValues = arr.sort(compare)
+	console.log(sortedValues)
+	// console.log(concatedArr)
+}
 
-// keyValues(data)
+getQuestion(11, data)
